4a10ffd0a59373f224a88785b4fd5b8d0405c7bc
Lukasz Piepiora <lpiepiora@gmail.com>
2024-01-14 06:31:18 +0100

    Adjust probe position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1574,7 +1574,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -39, -12, 0 }  // CR Touch Default Mount
+#define NOZZLE_TO_PROBE_OFFSET { -47, -13, 0 }  // CR Touch Default Mount
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
